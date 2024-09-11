307333d130e3256f8f78a9a191ad1ba94905c488
ghm7 <pozzisgnosis@outlook.com>
2024-09-11 10:29:49 -0300

    Punto 1

--- js/categories.js
+++ js/categories.js
@@ -63,7 +63,7 @@ function showCategoriesList() {
         (maxCount != undefined && parseInt(category.productCount) <= maxCount))
     ) {
       htmlContentToAppend += `
-            <div class="list-group-item list-group-item-action cursor-active">
+            <div id="${category.id}" class="list-group-item list-group-item-action cursor-active">
                 <div class="row">
                     <div class="col-3">
                         <img src="${category.imgSrc}" alt="${category.description}" class="img-thumbnail">
