e395ae10c766c99239e8fc2c76dee8b9abb8aa20
ghm7 <pozzisgnosis@outlook.com>
2024-09-11 20:16:00 -0300

    Add eslint ignore rules & comment unused code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,4 +4,5 @@ import pluginJs from '@eslint/js';
 export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
+  { ignores: ['js/dropzone.js', 'js/bootstrap.bundle.min.js'] },
 ];
